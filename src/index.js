4ad6e38d98eaa874aab152b412fe76f90dcb7f11
VladMironchuk <vlad.mironchuk.02@gmail.com>
2021-01-16 14:59:04 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  return arr==undefined || arr.length == 0 ? 0 : arr.sort((a,b)=>a-b)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  return arr==undefined || arr.length == 0 ? 0 : arr.sort((a,b)=>a-b).reverse()[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  return arr==undefined || arr.length == 0 ? 0 : arr.reduce((a,b) => a+b,0) / arr.length;
 }
